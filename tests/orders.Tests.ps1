acd4d4966cd22c7a60f5f8e6bae297c0287678eb
Hayot Olivier <olivier.hayot.dev@gmail.com>
2025-03-09 22:36:41 +0100

    added check

--- tests/orders.Tests.ps1
+++ tests/orders.Tests.ps1
@@ -5,6 +5,8 @@
 Describe 'Orders Tests' {
     It 'Given no parameters, it should insert empty Order' {
         Write-Host $PSScriptRoot
+        $scriptPath = Resolve-Path "$PSScriptRoot\..\src\Models\Order.ps1"
+        Write-Host "Loading script: $scriptPath"
         $order = Get-Order
         $order.generateInsertQuery() | Should Be "INSERT INTO [Order] (Customer, date) values(0, NULL)"
     }
